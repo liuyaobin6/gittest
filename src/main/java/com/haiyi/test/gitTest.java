8fa96357ca5fd0470c1506b43193c934c52d2ba4
lyb <lyb@qq.com>
2023-08-23 11:38:09 +0800

    hot-fix commit1

--- src/main/java/com/haiyi/test/gitTest.java
+++ src/main/java/com/haiyi/test/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("3");
         System.out.println("4");
+        System.out.println("hot-fix");
     }
 }
