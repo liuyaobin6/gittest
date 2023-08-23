514de2ff54cda5617cce940fab46a53a90fd4b16
liuyaobin6 <86583055+liuyaobin6@users.noreply.github.com>
2023-08-23 13:42:37 +0800

    Update gitTest.java

--- src/main/java/com/haiyi/test/gitTest.java
+++ src/main/java/com/haiyi/test/gitTest.java
@@ -7,5 +7,6 @@ public class gitTest {
         System.out.println("hot-fix");
         System.out.println("master");
         System.out.println("hothhhhhh");
+        System.out.println("hothhhhhh");
     }
 }
